68af8824291cf0570069f460095130f255cfc921
Theo Dedeken <theo.dedeken@telenet.be>
2020-04-22 09:57:25 +0200

    Bugfix pixelation

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import BootScene from './scenes/BootScene';
 const config = {
     // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
     type: Phaser.WEBGL,
-    pixelArt: true,
-    roundPixels: true,
     parent: 'content',
     width: 1280,
     height: 800,
